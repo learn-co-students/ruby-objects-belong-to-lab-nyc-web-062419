1d99fe021bed58dd9727ee5a1d04b989c28b11e1
Dave Bushnell <dybushnell@gmail.com>
2019-06-26 16:27:45 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+
+    attr_reader :name
+
+    #def initialize
+    #    @name
+    #end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+
+    attr_accessor :title, :artist
+
+    def initialize
+        @title
+        @artist
+    end
+
+    def title
+        @title
+    end
+
+    def artist
+        @artist
+    end
+
+
+
+end
