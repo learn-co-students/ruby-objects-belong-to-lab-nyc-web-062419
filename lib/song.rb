6d2ddf8e458f46472eebcfd33305c81dbd4a6071
yoseflehrer <yolehrer@gmail.com>
2019-06-27 23:06:43 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+
+    attr_accessor :title
+    def initialize
+        @title = title
+    end
+    
+    def artist=(artist)
+        @artist = artist
+    end
+    def artist
+        @artist
+    end
+    
+end
